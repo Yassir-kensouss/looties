06ccb5ada46e044b3eeea554fe54156d1de6ace8
yqssirkensouss <y.kensouss@qalqul.io>
2023-05-09 21:46:01 -0500

    updates

--- src/app/components/checkout/CheckoutPreview.jsx
+++ src/app/components/checkout/CheckoutPreview.jsx
@@ -1,7 +1,8 @@
-import { Formik } from "formik";
+"use client";
 import Image from "next/image";
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import Link from "next/link";
 
 const CheckoutPreview = () => {
   return (
@@ -30,7 +31,7 @@ const CheckoutPreview = () => {
             <p className="text-sm text-gray-900">x1</p>
           </div>
         </div>
-        <div>
+        <div className="pb-6">
           <h3 className="my-3 text-base font-semibold text-gray-800">
             Discount Code
           </h3>
@@ -55,18 +56,61 @@ const CheckoutPreview = () => {
             }}
           >
             <Form>
-              <div className="flex">
+              <div className="flex gap-4 flex-center">
                 <Field
-                  id="full_name"
-                  type="full_name"
-                  name="full_name"
+                  id="discount_code"
+                  type="text"
+                  name="discount_code"
                   placeholder="Add discount code"
                   className="custom-input"
                 />
-                <button>Apply</button>
+                <button
+                  style={{ padding: "8px 20px" }}
+                  className="text-base font-medium transition hover:bg-zinc-50 active:bg-white focus:outline focus:outline-zinc-300 text-zinc-700 rounded-lg border-2 border-zinc-300"
+                >
+                  Apply
+                </button>
               </div>
             </Form>
           </Formik>
+          <p className="mt-3 text-sm">
+            <span className="font-semibold text-gray-900">New customer? </span>
+            <Link href="/signup" className="underline decoration-1">
+              Sign up
+            </Link>{" "}
+            to get better offer
+          </p>
+        </div>
+        <div className="py-5">
+          <ul>
+            <li className="flex items-center justify-between py-1">
+              <div className="text-sm font-medium text-zinc-700">Subtotal</div>
+              <div className="font-semibold text-gray-800 text-sm">
+                $2,5000.00
+              </div>
+            </li>
+            <li className="flex items-center justify-between py-1">
+              <div className="text-sm font-medium text-zinc-700">Discount</div>
+              <div className="font-semibold text-gray-800 text-sm">-$0</div>
+            </li>
+            <li className="flex items-center justify-between">
+              <div className="text-sm font-medium text-zinc-700">
+                Shipment cost
+              </div>
+              <div className="font-semibold text-gray-800 text-sm">$22.5</div>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <li className="flex items-center justify-between pt-3">
+            <div className="text-base font-medium text-zinc-700">
+              Grand Total
+            </div>
+            <div className="font-semibold text-gray-800 text-lg">$2,522.50</div>
+          </li>
+          <button className="bg-gray-800 rounded-lg text-white p-3 w-full mt-4 transition hover:bg-gray-700 active:bg-gray-900 focus:outline focus:outline-zinc-300">
+            Continue to payment
+          </button>
         </div>
       </div>
     </div>
