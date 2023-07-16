257567af8aa527e3df530ea507e28500ce21977a
yqssirkensouss <y.kensouss@qalqul.io>
2023-07-15 21:18:40 -0500

    rm eslint

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ["res.cloudinary.com"],
   },
